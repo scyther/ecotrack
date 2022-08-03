fa339f9d02670fd50f27a9d1f18448ff1ed6925f
Chirag Sharma <sharma.chirag3737@gmail.com>
2022-08-03 13:30:22 +0530

    added S3 Storage

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,24 +1,61 @@
+import { Loader } from "@aws-amplify/ui-react";
+import { Storage } from "aws-amplify";
+import { useState } from "react";
 import logo from "../../logo.svg";
 import "./Home.css";
 
 const Home = () => {
+  const [showLoader, setshowLoader] = useState(false);
+  const [percentage, setPercentage] = useState(0);
+
+  //Image Upload
+  const uploadImage = async (e) => {
+    if (!e.target.files[0]) return;
+    const image = e.target.files[0];
+    setshowLoader(true);
+    await Storage.put(image.name, image, {
+      level: "private",
+      resumable: true,
+      progressCallback(progress) {
+        console.log(progress)
+        setPercentage(Math.floor((progress.loaded / progress.total) * 100));
+      },
+      completeCallback: (event) => {
+        console.log(`Successfully uploaded ${event.key}`);
+        setPercentage(0);
+        setshowLoader(false);
+      },
+    });
+  };
+
   return (
     <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Click a Photograph of the Product</p>
-      <label htmlFor="file-upload" className="Custom-file-upload">
-        Upload Photo
-      </label>
-      <input
-        id="file-upload"
-        capture="environment"
-        type="file"
-        accept="image/*"
-      />
-    </header>
-  </div>
-  )
-}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Click a Photograph of the Product</p>
+        <label htmlFor="file-upload" className="Custom-file-upload">
+          Upload Photo
+        </label>
+        <input
+          id="file-upload"
+          capture="environment"
+          type="file"
+          accept="image/*"
+          onChange={uploadImage}
+        />
+        {showLoader && (
+          <div className="loader">
+            <Loader
+              variation="linear"
+              percentage={percentage}
+              isDeterminate
+              width={300}
+            />
+          </div>
+        )}
+      </header>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/routes/Product/index.js
+++ src/routes/Product/index.js
@@ -1,65 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import './product.css';
-import { API } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import { listProducts } from '../../graphql/queries';
-import { createProduct as createProductMutation, deleteProduct as deleteProductMutation } from '../../graphql/mutations';
+import React, { useState, useEffect } from "react";
+import "./product.css";
+import { API, Storage } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { listProducts } from "../../graphql/queries";
+import {
+  createProduct as createProductMutation,
+  deleteProduct as deleteProductMutation,
+} from "../../graphql/mutations";
 
-const initialFormState = { name: '', description: '' }
+const initialFormState = { name: "", description: "" };
 
 const Product = () => {
-  const [notes, setNotes] = useState([]);
+  const [products, setProducts] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
-    fetchNotes();
+    fetchProducts();
   }, []);
 
-  async function fetchNotes() {
+  const onChange = async (e) => {
+    if (!e.target.files[0]) return;
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchProducts();
+  };
+  async function fetchProducts() {
     const apiData = await API.graphql({ query: listProducts });
-    setNotes(apiData.data.listProducts.items);
+    const productsfromAPI = apiData.data.listProducts.items;
+    await Promise.all(
+      productsfromAPI.map(async (product) => {
+        if (product.image) {
+          const image = await Storage.get(product.image);
+          product.image = image;
+        }
+        return product;
+      })
+    );
+    setProducts(apiData.data.listProducts.items);
   }
 
   async function createProduct() {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createProductMutation, variables: { input: formData } });
-    setNotes([ ...notes, formData ]);
+    await API.graphql({
+      query: createProductMutation,
+      variables: { input: formData },
+    });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setProducts([...products, formData]);
     setFormData(initialFormState);
   }
 
   async function deleteProduct({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteProductMutation, variables: { input: { id } }});
+    const newproductsArray = products.filter((product) => product.id !== id);
+    setProducts(newproductsArray);
+    await API.graphql({
+      query: deleteProductMutation,
+      variables: { input: { id } },
+    });
   }
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>My products App</h1>
       <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        placeholder="product name"
         value={formData.name}
       />
       <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
+        onChange={(e) =>
+          setFormData({ ...formData, description: e.target.value })
+        }
+        placeholder="product description"
         value={formData.description}
       />
-      <button onClick={createProduct}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteProduct(note)}>Delete note</button>
-            </div>
-          ))
-        }
+      <input type="file" onChange={onChange} />
+      <button onClick={createProduct}>Create Product</button>
+      <div style={{ marginBottom: 30 }}>
+        {products.map((product) => (
+          <div key={product.id || product.name}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <button onClick={() => deleteProduct(product)}>Delete Product</button>
+            {product.image && <img alt="Product" src={product.image} style={{width: 400}} />}
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default withAuthenticator(Product);
